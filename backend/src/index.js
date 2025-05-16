35266d52a41d3a3eacfcca3165a0e37013491525
Akshat Jain <akshatjpr03@gmail.com>
2025-05-16 10:17:14 +0530

    Setup LeetLab

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("Welcome to leetlab 🔥")
+})
+
+app.use("/api/v1/auth",authRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server is running on port 8080")
+})
