0fa99fad820907b218d6a529b1b2a4a114eeea4a
Akshat Jain <akshatjpr03@gmail.com>
2025-05-21 13:51:13 +0530

    import reactRouterDom & wrap app in main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   return (
     <>
+    <div className="text-3xl font-bold text-indigo-300">App</div>
     </>
   )
 }
